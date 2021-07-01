a53fcccad761e63a209c13a842a0935cc0312a26
Aaron Alva Caffo <caffo_aaron@hotmail.com>
2021-07-01 18:21:46 -0500

    pipeline declare

--- Backend/nodePipeline.go
+++ Backend/nodePipeline.go
@@ -5,27 +5,32 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
 var remotehost string
 
-func main() {
-	gin := bufio.NewReader(os.Stdin)
-	fmt.Print("Remote host: ")
-	remotehost, _ = gin.ReadString('\n')
-	remotehost = strings.TrimSpace(remotehost)
-	for {
-		fmt.Print("Enter number: ")
-		str, _ := gin.ReadString('\n')
-		num, _ := strconv.Atoi(strings.TrimSpace(str))
-		send(num)
-	}
-}
-
-func send(num int) {
+func enviar(num int) {
 	conn, _ := net.Dial("tcp", remotehost)
 	defer conn.Close()
+	//enviar el numero
 	fmt.Fprintf(conn, "%d\n", num)
+
+}
+
+func main() {
+
+	bufferIn := bufio.NewReader(os.Stdin)
+	fmt.Print("Ingrese el puerto remoto: ")
+	puerto, _ := bufferIn.ReadString('\n')
+	puerto = strings.TrimSpace(puerto)
+	remotehost = fmt.Sprintf("localhost:%s", puerto)
+
+	//enviar los datos al nodo 1 (8000)
+	enviar(35)
+	enviar(10)
+	enviar(25)
+	enviar(15)
+	enviar(30)
+
 }
